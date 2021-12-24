b976f5feeff62b8cfbc3b26241d27f5972ec6f87
Ayoub Driouich <adriouic@e1r10p11.1337.ma>
2021-12-24 16:47:29 +0100

    fresh fish

--- tools/cc.sh
+++ tools/cc.sh
@@ -5,9 +5,12 @@ gcc -Wall -Werror -Wextra -c push_swap.c ./utils/*.c
 #gcc  push_swap.o actions.o push_swap_utils.o -L. -lft -o push_swap
 gcc  *.o -L. -lft -o push_swap
 rm *.o
-echo "Runig /usr/local/bin/python3 random1.py"
+
+#echo "Runig /usr/local/bin/python3 random1.py"
 var=$(/usr/local/bin/python3 ./tools/random1.py)
-echo "Random numbers : "
-echo $var
-echo "Runing ./push_swap.py"
-./push_swap $var
+#echo "Random numbers : "
+#echo $var
+#echo "Runing ./push_swap.py"
+./push_swap $var |./checker_Mac $var
+./push_swap $var | wc -l
+#./push_swap $var
